d595650d7a821586d05bb1a613e004d4d3df9f2c
xujhone <xujhone@gmail.com>
2021-10-04 20:49:56 -0500

    update a O(n^2) solution

--- 375. Guess Number Higher or Lower II.py
+++ 375. Guess Number Higher or Lower II.py
@@ -1,6 +1,6 @@
+from collections import deque
 class Solution:
     def getMoneyAmount(self, n: int) -> int:
-
         # let dp[i][j] be min money needed to win for number from i to j
         # base cases:
         # dp[i][i] = 0, dp[i][j] = TBD if i > j
@@ -10,21 +10,26 @@ class Solution:
         # thus, dp[i][j] = 0 if i > j
         # thus, dp[i][j] = min_{i<=k<=j} k + max(dp[i][k-1], dp[k+1][j])
 
-        # dp[i][j+1] = min_{i<=k<=j+1} k + max(dp[i][k-1], dp[k+1][j+1])
-        #            = min(min_{i<=k<=j} k + max(dp[i][k-1], dp[k+1][j+1]), j+1 + max(dp[i][j], dp[j+2][j+1]))
-        #            = min(min_{i<=k<=j} k + max(dp[i][k-1], dp[k+1][j+1]), j+1 + dp[i][j])
-        # inductively, suppose dp[k+1][j+1] >= dp[k+1][j], then dp[i][j+1] >= dp[i][j]
-        # Thus, dp[i][j] increases as j increases
-        # similarly, dp[i][j] decreases as i increases
+        dp = [[0] * (n + 1) for _ in range(n + 1)]
 
-        #
+        for j in range(2, n + 1):
+            # let k0 be the largest k such that dp[i][k-1] <= dp[k+1][j]
+            # when i = j - 1, dp[j-1][k-1] <= dp[k+1][j], take k0 = j - 1
+            k = j - 1
+            # deque of (dec, inc) pairs (k, k + dp[k+1][j]) for i <= k <= k0
+            dq = deque()
+            for i in range(j - 1, 0, -1):
+                # note as i decreases k0 decreases
+                while dp[i][k - 1] > dp[k + 1][j]:
+                    k -= 1
+                    if dq and dq[0][0] > k:
+                        dq.popleft()
+                # add (i, i + dp[i+1][j]) to deque
+                val = i + dp[i + 1][j]
+                while dq and dq[-1][1] >= val:
+                    dq.pop()
+                dq.append((i, val))
 
-        dp = [[0] * (n + 2) for _ in range(n + 2)]
-
-        for l in range(2, n + 1):
-            # l = j - i + 1
-            for i in range(1, n - l + 2):
-                j = l + i - 1
-                dp[i][j] = min(k + max(dp[i][k - 1], dp[k + 1][j]) for k in range(i, j + 1))
+                dp[i][j] = min(dq[0][1], dp[i][k] + k + 1)
 
         return dp[1][n]
